7a5a49f0d53e52398e889f26865023b58e1026e1
Chad <chad.lines1@gmail.com>
2022-07-21 15:59:55 -0600

    0.0091 - Dashboard GUI Build (In Progress)

--- Views/Dashboard.Designer.cs
+++ Views/Dashboard.Designer.cs
@@ -28,36 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabHome = new System.Windows.Forms.TabPage();
+            this.tabProjects = new System.Windows.Forms.TabPage();
+            this.tabAllIssues = new System.Windows.Forms.TabPage();
+            this.tabForecast = new System.Windows.Forms.TabPage();
+            this.tabReports = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabSettings = new System.Windows.Forms.TabPage();
+            this.toolTipProject = new System.Windows.Forms.ToolTip(this.components);
+            this.materialTabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // materialTabControl1
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Size = new System.Drawing.Size(1223, 639);
-            this.splitContainer1.SplitterDistance = 923;
-            this.splitContainer1.TabIndex = 0;
+            this.materialTabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.materialTabControl1.Controls.Add(this.tabHome);
+            this.materialTabControl1.Controls.Add(this.tabProjects);
+            this.materialTabControl1.Controls.Add(this.tabAllIssues);
+            this.materialTabControl1.Controls.Add(this.tabForecast);
+            this.materialTabControl1.Controls.Add(this.tabReports);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.materialTabControl1.ImageList = this.imageList1;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Multiline = true;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(295, 607);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // tabHome
+            // 
+            this.tabHome.ImageKey = "home.png";
+            this.tabHome.Location = new System.Drawing.Point(31, 4);
+            this.tabHome.Name = "tabHome";
+            this.tabHome.Size = new System.Drawing.Size(260, 599);
+            this.tabHome.TabIndex = 4;
+            this.tabHome.Text = "Home";
+            this.tabHome.UseVisualStyleBackColor = true;
+            // 
+            // tabProjects
+            // 
+            this.tabProjects.ImageKey = "projects.png";
+            this.tabProjects.Location = new System.Drawing.Point(31, 4);
+            this.tabProjects.Name = "tabProjects";
+            this.tabProjects.Padding = new System.Windows.Forms.Padding(3);
+            this.tabProjects.Size = new System.Drawing.Size(260, 599);
+            this.tabProjects.TabIndex = 0;
+            this.tabProjects.Text = "Projects";
+            this.toolTipProject.SetToolTip(this.tabProjects, "Projects");
+            this.tabProjects.UseVisualStyleBackColor = true;
+            // 
+            // tabAllIssues
+            // 
+            this.tabAllIssues.ImageKey = "tasks.png";
+            this.tabAllIssues.Location = new System.Drawing.Point(31, 4);
+            this.tabAllIssues.Name = "tabAllIssues";
+            this.tabAllIssues.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAllIssues.Size = new System.Drawing.Size(260, 599);
+            this.tabAllIssues.TabIndex = 1;
+            this.tabAllIssues.Text = "All Issues";
+            this.tabAllIssues.UseVisualStyleBackColor = true;
+            // 
+            // tabForecast
+            // 
+            this.tabForecast.ImageKey = "calendar.png";
+            this.tabForecast.Location = new System.Drawing.Point(31, 4);
+            this.tabForecast.Name = "tabForecast";
+            this.tabForecast.Size = new System.Drawing.Size(260, 599);
+            this.tabForecast.TabIndex = 3;
+            this.tabForecast.Text = "Forecast";
+            this.tabForecast.UseVisualStyleBackColor = true;
+            // 
+            // tabReports
+            // 
+            this.tabReports.ImageKey = "reports.png";
+            this.tabReports.Location = new System.Drawing.Point(31, 4);
+            this.tabReports.Name = "tabReports";
+            this.tabReports.Size = new System.Drawing.Size(260, 599);
+            this.tabReports.TabIndex = 2;
+            this.tabReports.Text = "Reports";
+            this.tabReports.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "calendar.png");
+            this.imageList1.Images.SetKeyName(1, "export.png");
+            this.imageList1.Images.SetKeyName(2, "projects.png");
+            this.imageList1.Images.SetKeyName(3, "reports.png");
+            this.imageList1.Images.SetKeyName(4, "settings.png");
+            this.imageList1.Images.SetKeyName(5, "tasks.png");
+            this.imageList1.Images.SetKeyName(6, "home.png");
+            // 
+            // tabSettings
+            // 
+            this.tabSettings.ImageIndex = 2;
+            this.tabSettings.Location = new System.Drawing.Point(39, 4);
+            this.tabSettings.Name = "tabSettings";
+            this.tabSettings.Size = new System.Drawing.Size(252, 599);
+            this.tabSettings.TabIndex = 4;
+            this.tabSettings.UseVisualStyleBackColor = true;
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1223, 639);
-            this.Controls.Add(this.splitContainer1);
+            this.ClientSize = new System.Drawing.Size(1336, 674);
+            this.Controls.Add(this.materialTabControl1);
+            this.DrawerShowIconsWhenHidden = true;
+            this.DrawerTabControl = this.materialTabControl1;
             this.Name = "Dashboard";
             this.Text = "Dashboard";
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.materialTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private SplitContainer splitContainer1;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private TabPage tabProjects;
+        private TabPage tabAllIssues;
+        private TabPage tabForecast;
+        private TabPage tabReports;
+        private ImageList imageList1;
+        private TabPage tabSettings;
+        private ToolTip toolTipProject;
+        private TabPage tabHome;
     }
 }
--- Views/Dashboard.cs
+++ Views/Dashboard.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace Project_Management.Views
 {
-    public partial class Dashboard : Form
+    public partial class Dashboard : MaterialForm
     {
         public Dashboard()
         {
             InitializeComponent();
+
+            // Setting up the Material Skin Manager
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue900, 
+                Primary.Blue900, Primary.Blue500, Accent.Blue700, 
+                TextShade.WHITE);
+
+
         }
     }
 }
